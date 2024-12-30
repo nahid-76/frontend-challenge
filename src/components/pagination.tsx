e927af41384b2fb1f71d99e43a432c81c9a40dfa
nahid-76 <nahidpagheh1@gmail.com>
2024-12-30 13:13:19 +0330

    launches/dot style added to pagination

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -8,6 +8,7 @@ interface Props {
   page: number;
   searchParams: Record<string, string>;
 }
+
 export const Pagination: React.FC<Props> = ({
   hasPrevPage,
   totalPages,
@@ -15,6 +16,97 @@ export const Pagination: React.FC<Props> = ({
   page,
   searchParams,
 }) => {
+  const linkClasses =
+    "transition-transform duration-300 p-2 rounded-md mx-1 size-8 flex items-center justify-center cursor-pointer mt-1";
+  const renderPages = () => {
+    const pages = [];
+
+    const startPage = Math.max(1, page - 1);
+    const endPage = Math.min(totalPages, page + 1);
+
+    for (let i = 1; i <= totalPages; i++) {
+      if (i === 1) {
+        pages.push(
+          <Link
+            href={{
+              pathname: "/",
+              query: { ...searchParams, page: i },
+            }}
+            key={i}
+            className={classNames(
+              linkClasses,
+              page === i
+                ? "bg-gray-400 transform scale-125"
+                : "bg-gray-800 text-white"
+            )}
+          >
+            {i}
+          </Link>
+        );
+
+        if (startPage > 2) {
+          pages.push(
+            <span
+              key="dots-start"
+              className="p-2 mx-1 size-8 flex items-center justify-center mt-1 text-gray-500"
+            >
+              ...
+            </span>
+          );
+        }
+      } else if (i >= startPage && i <= endPage) {
+        pages.push(
+          <Link
+            href={{
+              pathname: "/",
+              query: { ...searchParams, page: i },
+            }}
+            key={i}
+            className={classNames(
+              linkClasses,
+              page === i
+                ? "bg-gray-400 transform scale-125"
+                : "bg-gray-800 text-white"
+            )}
+          >
+            {i}
+          </Link>
+        );
+      } else if (i === totalPages) {
+        if (endPage < totalPages - 1) {
+          pages.push(
+            <span
+              key="dots-end"
+              className="p-2 mx-1 size-8 flex items-center justify-center mt-1 text-gray-500"
+            >
+              ...
+            </span>
+          );
+        }
+
+        pages.push(
+          <Link
+            href={{
+              pathname: "/",
+              query: { ...searchParams, page: i },
+            }}
+            key={i}
+            className={classNames(
+              linkClasses,
+              page === i
+                ? "bg-gray-400 transform scale-125"
+                : "bg-gray-800 text-white"
+            )}
+          >
+            {i}
+          </Link>
+        );
+      }
+    }
+
+    return pages;
+  };
+
   return (
     <div className="mt-3 flex align-middle justify-center flex-wrap">
       <Link
@@ -32,23 +124,8 @@ export const Pagination: React.FC<Props> = ({
         {"<"}
       </Link>
 
-      {Array.from(new Array(totalPages ?? 1)).map((_, i) => (
-        <Link
-          href={{
-            pathname: "/",
-            query: { ...searchParams, page: i + 1 },
-          }}
-          key={i}
-          className={classNames(
-            "transition-transform duration-300 p-2 rounded-md mx-1 size-8 flex items-center justify-center cursor-pointer mt-1",
-            page === i + 1
-              ? "bg-gray-400 transform scale-125"
-              : "bg-gray-800 text-white"
-          )}
-        >
-          {i + 1}
-        </Link>
-      ))}
+      {renderPages()}
+
       <Link
         href={{
           pathname: "/",
