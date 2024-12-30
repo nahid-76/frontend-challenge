fe3b31e5b7b9c1bf735e7709c9dafa31e1750f99
nahid-76 <nahidpagheh1@gmail.com>
2024-12-30 10:55:11 +0330

    launches/RootLoading implemented

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from "@/components";
+
+export default function RootLoading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Spinner />
+    </div>
+  );
+}
