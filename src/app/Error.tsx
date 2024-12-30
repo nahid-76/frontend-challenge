a218f176c3edf78cb8f58d012f937263902d8613
nahid-76 <nahidpagheh1@gmail.com>
2024-12-30 10:55:59 +0330

    launches/error server component converted to client component

--- src/app/Error.tsx
+++ src/app/Error.tsx
@@ -1,8 +1,9 @@
+"use client"
 import React from 'react';
 
 const Error = () => {
   return (
-    <div className={"flex items-center justify-center h-80 bg-red-400 border-red-950 rounded-md flex-col"}>
+    <div className={"flex items-center justify-center h-80 bg-red-400 border-red-950 flex-col"}>
       <div className="text-black">There was an error</div>
       <button
         className="text-center text-red-950 border mt-3 py-1 px-3 rounded-md bg-transparent border-red-950 hover:bg-red-950 hover:text-white"
